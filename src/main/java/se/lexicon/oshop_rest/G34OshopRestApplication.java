7e859bee2efed11b1b4a86dc0c440dcf7b470636
Mehrdad Javan <mehrdad.javan@adapta.se>
2021-05-03 09:08:22 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/oshop_rest/G34OshopRestApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.oshop_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G34OshopRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G34OshopRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/oshop_rest/G34OshopRestApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.oshop_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G34OshopRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
